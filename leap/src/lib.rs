77959e246540459ccf322f2d7d56b11f5df45270
kkpoon <noopkk@gmail.com>
2017-01-29 15:05:28 +0800

    leap 2

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,11 +1,8 @@
-pub fn is_leap_year(year: i32) -> bool {
-    if year % 400 == 0 {
-        true
-    } else if year % 100 == 0 {
-        false
-    } else if year % 4 == 0 {
-        true
-    } else {
-        false
+pub fn is_leap_year(year: u16) -> bool {
+    match year {
+        x if x % 400 == 0 => true,
+        x if x % 100 == 0 => false,
+        x if x % 4 == 0 => true,
+        _ => false,
     }
 }
