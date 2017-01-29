adc0a52eda749fd9b9218d1ff14d789b9d2fb564
kkpoon <noopkk@gmail.com>
2017-01-29 15:00:46 +0800

    leap

---
+++ leap/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn is_leap_year(year: i32) -> bool {
+    if year % 400 == 0 {
+        true
+    } else if year % 100 == 0 {
+        false
+    } else if year % 4 == 0 {
+        true
+    } else {
+        false
+    }
+}
