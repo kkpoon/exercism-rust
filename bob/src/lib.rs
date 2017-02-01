0496ddbe58cb12a1173749b7f94ea7c17e22e84f
kkpoon <noopkk@gmail.com>
2017-02-01 22:00:18 +0800

    better return type on bob

--- bob/src/lib.rs
+++ bob/src/lib.rs
@@ -1,9 +1,8 @@
-pub fn reply(input: &str) -> String {
+pub fn reply(input: &str) -> &str {
     match input.trim() {
-            x if x.ends_with("?") => "Sure.",
-            x if x.is_empty() => "Fine. Be that way!",
-            x if x.to_uppercase() == x => "Whoa, chill out!",
-            _ => "Whatever.",
-        }
-        .to_string()
+        x if x.ends_with("?") => "Sure.",
+        x if x.is_empty() => "Fine. Be that way!",
+        x if x.to_uppercase() == x => "Whoa, chill out!",
+        _ => "Whatever.",
+    }
 }
