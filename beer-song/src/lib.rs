564bda55cb5660b64ba388593996e02f35609ef2
kkpoon <noopkk@gmail.com>
2017-02-02 22:37:45 +0800

    beer-song2

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,24 +1,24 @@
 fn bottle(n: u32) -> String {
     match n {
-        x if x == 0 => format!("No more bottles of beer on the wall, no more bottles of beer"),
-        x if x == 1 => format!("{} bottle of beer on the wall, {} bottle of beer", n, n),
+        0 => format!("No more bottles of beer on the wall, no more bottles of beer"),
+        1 => format!("{} bottle of beer on the wall, {} bottle of beer", n, n),
         _ => format!("{} bottles of beer on the wall, {} bottles of beer", n, n),
     }
 }
 
 fn action(n: u32) -> String {
     match n {
-        x if x == 0 => "Go to the store and buy some more".to_string(),
-        x if x == 1 => "Take it down and pass it around".to_string(),
+        0 => "Go to the store and buy some more".to_string(),
+        1 => "Take it down and pass it around".to_string(),
         _ => "Take one down and pass it around".to_string(),
     }
 }
 
 fn after(n: u32) -> String {
     match n {
-        x if x == 0 => "99 bottles of beer on the wall".to_string(),
-        x if x == 1 => "no more bottles of beer on the wall".to_string(),
-        x if x == 2 => "1 bottle of beer on the wall".to_string(),
+        0 => "99 bottles of beer on the wall".to_string(),
+        1 => "no more bottles of beer on the wall".to_string(),
+        2 => "1 bottle of beer on the wall".to_string(),
         _ => format!("{} bottles of beer on the wall", n - 1),
     }
 }
